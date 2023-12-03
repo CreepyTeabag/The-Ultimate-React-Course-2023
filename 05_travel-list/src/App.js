51179e440c42ede68be176282c866e85ec34f346
CreepyTeabag <124189089+CreepyTeabag@users.noreply.github.com>
2023-12-03 20:00:58 +0300

    Watched 6.11 - 6.15

---
+++ 05_travel-list/src/App.js
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you ned for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X items o your list and you already packed X (X%)</em>
+    </footer>
+  );
+}
